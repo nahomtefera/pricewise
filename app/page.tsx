6176cad213796cc7a86ed5de34efda96ea0dda22
nahom <nahumtefera@gmail.com>
2023-10-03 21:18:25 -0400

    added code to retrieve allProducts or a single product by id, created ProductCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import HeroCarousel from "@/components/heroCarousel"
 import SearchBar from "@/components/searchBar"
 import Image from "next/image"
+import { getAllProducts } from "@/libs/actions"
+import ProductCard from "@/components/productCard"
 
-const Home = () => {
+const Home = async () => {
+
+  const allProducts = await getAllProducts();
+
+
   return (
     <>
       <section className="px-6 md:px-20 py-24 border-2 border-white">
@@ -39,8 +45,8 @@ const Home = () => {
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {
-            ['Apple Iphone15', 'Book', 'Sneakers'].map((product) => (
-              <div>{product}</div>
+            allProducts?.map((product) => (
+              <ProductCard key={product.id} product={product} />
             ))
           }
         </div>
