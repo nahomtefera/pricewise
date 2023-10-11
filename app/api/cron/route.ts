50fa09c67e7aef4845803146bfdfbd492e46f6e5
nahom <nahumtefera@gmail.com>
2023-10-11 16:02:51 -0400

    fix error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { scrapeAmazonProduct } from "@/libs/scrape";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/libs/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 10;
+export const maxDuration = 300;
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
